13b4a1ed5798660b7d2080577fa1ca921027a902
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-13 18:10:34 -0300

    fix: remove nullable isAdmin of user

--- Backend.Api/Models/User.cs
+++ Backend.Api/Models/User.cs
@@ -13,6 +13,6 @@ namespace Backend.Api.Models
         public string? Name { get; set; }
         public string? Email { get; set; }
         public string? Password { get; set; }
-        public bool? IsAdmin { get; set; }
+        public bool IsAdmin { get; set; }
     }
 }
