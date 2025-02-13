25fd97006dd6d89cd7d2b4f991e91f5f64f659ff
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-13 14:43:26 -0300

    feat: add dependencies Userrepository, AuthService, UserService

--- Backend.Api/Program.cs
+++ Backend.Api/Program.cs
@@ -18,6 +18,9 @@ builder.Services.AddDbContext<ApplicationContext>(op => op.UseNpgsql(connectionS
 //Add repositories and services:
 builder.Services.AddScoped<PersonRepository>();
 builder.Services.AddScoped<PersonService>();
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<UserService>();
 builder.Services.AddMvc(options => options.Filters.Add(typeof(ExceptionFilter)));
 var app = builder.Build();
 
