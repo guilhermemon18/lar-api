84f521283b7aed8d00f77f57c055df1c74ed8b00
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-13 19:02:09 -0300

    refactor: remove prints about database connection test

--- Backend.Api/Context/ApplicationContext.cs
+++ Backend.Api/Context/ApplicationContext.cs
@@ -7,19 +7,6 @@ namespace Backend.Api.Context
 {
     public class ApplicationContext(DbContextOptions<ApplicationContext> options) : DbContext(options)
     {
-        public bool TestarConexao()
-        {
-            try
-            {
-                return Database.CanConnect();
-            }
-            catch (Exception ex)
-            {
-                // Log do erro (opcional)
-                Console.WriteLine($"Erro ao conectar ao banco de dados: {ex.Message}");
-                return false;
-            }
-        }
 
         public DbSet<Person> People { get; set; }
         public DbSet<Phone> Phones { get; set; }
