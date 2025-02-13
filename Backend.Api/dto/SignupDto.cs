fbf1b2cc1018eb7e1669d5c08c9fe3bc1d2fa96e
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-13 10:00:10 -0300

    feat: add SignupDto

---
+++ Backend.Api/dto/SignupDto.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Api.dto
+{
+    public class SignupDto
+    {
+        [Required]
+        [MinLength(2)]
+        public string? Name { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress]
+        public string? Email { get; set; }
+        [Required]
+        [MinLength(4)]
+        public string? Password { get; set; }
+
+    }
+}
