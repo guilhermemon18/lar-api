1ae4d7dcfa295120222a49f4925c591054c552d5
guilhermemon18 <guilhermemonsani7@gmail.com>
2025-02-13 09:44:16 -0300

    feat: add User model

---
+++ Backend.Api/Models/User.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Api.Models
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public bool? IsAdmin { get; set; }
+    }
+}
